2a8a38fa124872ff5d500ea18396aeb9542a7c5e
Anderson Torres Bessa <anderson.bessa@programmers.com.br>
2022-06-06 12:23:57 -0300

    Add existing file

---
+++ exercicio_nota_aluno/Aula_C/Program.cs
@@ -0,0 +1,59 @@
+﻿//exercicio usando if e else, e ternário também.
+
+//alunos que foram aprovados, recuperação e reprovados. Reprovados =< 5. De 5 a 6 de recuperação. Menor que 7 é reprovado.
+
+
+//USANDO IF ELSE
+using System;
+
+float nota = 0;
+
+List<float> listaDeIdade;
+listaDeIdade = new List<float>();
+
+
+Console.WriteLine("Digite a nota: ");
+float aluno = float.Parse(Console.ReadLine());
+listaDeIdade.Add(aluno);
+
+foreach (var i in listaDeIdade)
+{
+    if (i < 5)
+    {
+        Console.WriteLine("Reprovado");
+    };
+
+    if (i >= 5 && i <= 6)
+    {
+        Console.WriteLine("Recuperação");
+    };
+    if (i >= 7)
+    {
+        Console.WriteLine("Aprovado");
+    };
+
+}
+
+
+//USANDO TERNÁRIO
+/*
+using System;
+
+float nota = 0;
+
+List<float> listaDeIdade;
+listaDeIdade = new List<float>();
+
+
+Console.WriteLine("Digite a nota: ");
+float aluno = float.Parse(Console.ReadLine());
+listaDeIdade.Add(aluno);
+
+foreach (var i in listaDeIdade)
+{
+    var x = (i >= 7 ? "Aprovado" : "Reprovado");
+    Console.WriteLine(x);
+
+
+}
+*/
